145196daaab0c6be2337e5f4541c05cf05358ae0
RAHUL UJJAWAL SHARMA <143214926+rahulkumarujjawal@users.noreply.github.com>
2026-02-15 01:09:49 +0530

    Add Spark session creation and DataFrame example

--- PySpark/01_dataframe_basics.py
+++ PySpark/01_dataframe_basics.py
@@ -1,1 +1,9 @@
+# Create Spark Session
+from pyspark.sql import SparkSession
 
+spark = SparkSession.builder.appName("Practice").getOrCreate()
+
+data = [(1, "Rahul"), (2, "Amit")]
+df = spark.createDataFrame(data, ["id", "name"])
+
+df.show()
